8a5bdc45920c10b5961264b47418954ab5f8118b
gjj <465884236@qq.com>
2019-06-07 18:01:38 +0800

    修改显示位数

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -243,7 +243,7 @@ public class Calculator {
                     System.out.print(format.format(x) + " ");
                 } else {
                     if (x.toString().length() - x.toString().indexOf(".") > 10) {
-                        System.out.print(x.toString().substring(0, x.toString().indexOf(".") + 10) + " ");
+                        System.out.print(x.toString().substring(0, x.toString().indexOf(".") + 11) + " ");
                     } else {
                         System.out.print(x + " ");
                     }
